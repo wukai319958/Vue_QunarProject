f7749d324c103086aa9c453092a92fb79a231e4a
wukai319958 <945546895@qq.com>
2020-01-03 20:59:22 +0800

    项目初始化。引入reset.css，统一移动端基础样式 引入fastClick方案，消除移动端点击事件300ms延迟 引入border.css消除移动端1像素边框问题

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick' // 引入fastclick
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body) // 绑定到document.body
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router, // ES6写法相当于ruuter:router
   components: { App }, // ES6写法相当于components:{App:App}
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/', // 访问根目录下时，路由到HelloWorld.vue文件
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/', // 访问根目录下时，路由到HelloWorld.vue文件
+    name: 'Home',
+    component: Home
+  }]
 })
