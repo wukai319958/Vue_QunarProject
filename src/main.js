7325f9d06a0e81d1374300ff5b0cffdd94fc422d
wukai319958 <945546895@qq.com>
2020-01-05 15:24:04 +0800

    添加首页轮播图，引入vue-awesome-swiper轮播组件,定义一个数组，遍历数组对象，让轮播图显示出来（MVVM模式）

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick' // 引入fastclick
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import 'styles/iconfont.css'
 import 'styles/reset.css'
 import 'styles/border.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 绑定到document.body
+Vue.use(VueAwesomeSwiper /* {default global options} */)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+   </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: require('@/assets/Images/homeimage4.jpg')
+      },
+      {
+        id: '0002',
+        imgUrl: require('@/assets/Images/homeimage5.jpg')
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 26.7%
+  background-color #eee
+  .swiper-img
+    width 100%
+</style>
