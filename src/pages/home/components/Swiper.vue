0a5f0e62e7e8598aeba97ed43c0009b220b42cfe
wukai319958 <945546895@qq.com>
2020-01-10 00:00:49 +0800

    添加Mock数据，父子组件数据传递渲染子组件

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,21 +12,21 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true,
         autoplay: 8000
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: require('@/assets/Images/homeimage4.jpg')
-      },
-      {
-        id: '0002',
-        imgUrl: require('@/assets/Images/homeimage5.jpg')
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
@@ -39,7 +39,7 @@ export default {
   overflow hidden
   width 100%
   height 0
-  padding-bottom 26.7%
+  padding-bottom 31.2%
   background-color #eee
   .swiper-img
     width 100%
