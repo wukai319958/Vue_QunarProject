b30fc713137f8c146ba02a6c09a68af6a38fbd3d
wukai319958 <945546895@qq.com>
2020-01-12 14:38:28 +0800

    城市列表路由&列表头部编写

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #ffffff
+    background $bgColor
+    font-size .32rem
+    .header-back
+      width .64rem
+      font-size .4rem
+      text-align center
+      position absolute
+      top 0
+      left 0
+      color #ffffff
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/', // 访问根目录下时，路由到HelloWorld.vue文件
+    path: '/', // 访问根目录下时，路由到Home.vue文件
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/city', // 访问根目录下时，路由到Home.vue文件
+    name: 'City',
+    component: City
   }]
 })
